5924976a420de2aed5c58e7c54a80a2a9974585b
melSoftUni <mel.mockingjay@gmail.com>
2023-10-10 19:43:40 -0400

    reset user password functionality

--- social_website/settings.py
+++ social_website/settings.py
@@ -91,3 +91,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
