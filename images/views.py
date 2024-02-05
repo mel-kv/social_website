0849a8a62ca98041c12c0def599c6d65cfe5c54b
melSoftUni <milena.koeva@gmail.com>
2024-02-05 13:04:09 -0500

    added redis ranking images

--- images/views.py
+++ images/views.py
@@ -41,6 +41,7 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     total_views = r.incr(f'image:{image.id}:views')
+    r.zincrby('image_ranking', 1, image.id)
     return render(request,
                   '../templates/account/images/detail.html',
                   {'section': 'images',
@@ -88,3 +89,17 @@ def image_list(request):
                        'images': images})
     return render(request, '../templates/account/images/list.html', {'section': 'images',
                                                                      'images': images})
+
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'account/images/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
